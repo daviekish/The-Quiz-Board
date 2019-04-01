89f42ccd930186e42f675b930173807fec6da302
daviekish <davie.kamweru5612@gmail.com>
2019-04-01 12:31:33 +0300

    deletion of javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,15 @@
-$(function() {
-      $("#start-btn").click(function(event) {
+$(document).ready(function() {
+      var userFirstName = $("#first name").val();
+      var userSecondName = $("#second namme").val();
+      $("#start-btn").click(function() {
+          (event) {
             event.preventDefault();
-            var userFirstName = $("#firstName").val();
-            var userSecondName = $("#secondName").val();
-            if (userFirstName.length === 0 || userSecondName.length === 0) {
-              $("#no-name").show();
-              $("#firstName").addClass("enter-name");
-              $("#secondName").addClass("enter-name");
-            } else {
-
+            if (userFirstName.length === 0 || userSecondName.lenght === 0) {
+              $("no name").toggle();
             }
-
-            function scoreCalculator(userValue) {
-              var names = ["js", "year", "framework"];
-              names.forEach(function(name) {
-                var userValue = $("input:radio[name=" + name + "]:checked").val();
-                if (userValue === "10") {
-                  sum += 10;
-                }
-              });
-            }
+          });
+      });
+       $(document).ready(function() {
+        $("p").click(function() {
+          alert("This is a header.");
+        });
