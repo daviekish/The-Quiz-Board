96e70dc7336e2ca1022a4cf5e651f34a640b3ba0
daviekish <davie.kamweru5612@gmail.com>
2019-04-01 15:11:38 +0300

    anychanges made

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,39 @@
+//User Interface logic
 $(document).ready(function() {
-      var userFirstName = $("#first name").val();
-      var userSecondName = $("#second namme").val();
-      $("#start-btn").click(function() {
-          (event) {
-            event.preventDefault();
-            if (userFirstName.length === 0 || userSecondName.lenght === 0) {
-              $("#no-name").toggle();
-            }
-          });
-      });
-       $(document).ready(function() {
-        $("p").click(function() {
-          alert("This is a header.");
-        });
+  $("#test").click(function() {
+
+    var score = 0;
+    var userName = $("#user").val()
+    var answer = [];
+    answer.push($("input:radio[name=questionOne]:checked").val());
+    answer.push($("input:radio[name=questionTwo]:checked").val());
+    answer.push($("input:radio[name=questionThree]:checked").val());
+    answer.push($("input:radio[name=questionFour]:checked").val());
+    answer.push($("input:radio[name=questionFive]:checked").val());
+    $(".results").show();
+    $("#form1").hide(function(event) {
+      $("#results").html(userName + ", you scored " + parseInt((score / 5) * 100) + "%");
+    });
+
+    //Business logic
+    if (answer[0] == "correct") {
+      score++;
+    }
+    if (answer[1] == "correct") {
+      score++;
+    }
+    if (answer[2] == "correct") {
+      score++;
+    }
+    if (answer[3] == "correct") {
+      score++;
+    }
+    if (answer[4] == "correct") {
+      score++;
+    }
+  });
+});
+
+function reset() {
+  location.reload();
+}
